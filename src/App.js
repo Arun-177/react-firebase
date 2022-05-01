1742b5c1549a42aff25616f441cbbe3c61d50996
arun-177 <guleriaarun177@gmail.com>
2022-05-01 20:14:31 +0530

    mail removed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Casting Spell, Taking a little while</h1>
-      <h1>Get in touch <a href="mailto:arunsinghguleria@gmail.com">with me</a></h1>
     </div>
   );
 }
