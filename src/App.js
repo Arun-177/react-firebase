f49fa68bb9dd99dd7407758491bae70710f96263
arun-177 <guleriaarun177@gmail.com>
2022-05-04 19:38:16 +0530

    reversed last commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Casting Spell, Taking a little while</h1>
+      <h1>Get in touch <a href="mailto:arunsinghguleria@gmail.com">with me</a></h1>
     </div>
   );
 }
