a82207c20261a52b53c66625e601bf1572148131
Arun-177 <44317942+Arun-177@users.noreply.github.com>
2025-02-13 22:36:31 +0530

    valentine component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Game from './TicTacToe/Game'
 import Home from './home'
+import Valentine from './Valentine/App'
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
 
         <Route path="/" element={<Home />} />
         <Route path="/tictactoe" element={<Game />} />
+        <Route path="/valentine" element={<Valentine />} />
       </Routes>
     </Router>
   );
